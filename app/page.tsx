5dba6d2134150c0230a3a0823f54ad1157308d94
Alirexa <112307078+rostamialirexa@users.noreply.github.com>
2024-09-25 12:16:45 +0330

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='font-serif text-3xl font-bold'>NextJs Clerk Starter</h1>
+        <p className='mt-3 text-muted-foreground'>
+          A NextJs starter template with Clerk authentication, shadcn/ui and
+          Tailwind CSS.
+        </p>
+      </div>
+    </section>
+  )
+}
